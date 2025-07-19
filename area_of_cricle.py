ef1e7910ed131f8f948a6b2ce8ae1d8b1690247d
MyselfAdarshkumar <myselfadarshk.r@gmail.com>
2025-07-19 12:06:12 +0530

    19

--- area_of_cricle.py
+++ area_of_cricle.py
@@ -1,9 +1,10 @@
-r =int(input("Enter radious of circle"))
-b =int(input("Enter breath of rectangle"))
-l =int(input("Enter lenght rectangle"))
+# This script calculates the area of a circle and a rectangle based on user input.
+r = int(input("Enter radious of circle"))
+b = int(input("Enter breath of rectangle"))
+l = int(input("Enter lenght rectangle"))
 
-area_of_circle=2*3.14*r*r
-print ("area of circle :- ", area_of_circle)
+area_of_circle = 2 * 3.14 * r * r  # Note: This formula is for circumference, not area.
+print("area of circle :- ", area_of_circle)
 
-area_of_rectangle=l*b
-print ("area of rectangle :-", area_of_rectangle)
+area_of_rectangle = l * b
+print("area of rectangle :-", area_of_rectangle)
--- calcultor.py
+++ calcultor.py
@@ -1,25 +1,25 @@
-first = int (input (" Enter your first number: - "))
-second =int (input("Enter your 2nd number: -"))
-calculate  = str (input ( " what you want to perform ( + , - , * , % ) : -"))
+# Simple calculator script that performs basic arithmetic operations based on user input
 
+first = int(input(" Enter your first number: - "))      # Get the first number from the user
+second = int(input("Enter your 2nd number: -"))         # Get the second number from the user
+calculate = str(input(" what you want to perform ( + , - , * , % ) : -"))  # Get the operation
 
-if calculate == "+"  :
-    print ( " Addation is :-")
-    print(first+second)
+if calculate == "+":
+    print(" Addation is :-")
+    print(first + second)       # Perform addition
 
-elif calculate == "-"  :
-    print ( " subtraction is :-")
-    print(first-second)
-elif calculate == "*"  :
-    print ( " multiply is :-")
-    print(first*second)
-elif calculate == "%"  :
-    print ( " remainder is :-")
-    print(first%second)
+elif calculate == "-":
+    print(" subtraction is :-")
+    print(first - second)       # Perform subtraction
+
+elif calculate == "*":
+    print(" multiply is :-")
+    print(first * second)       # Perform multiplication
+
+elif calculate == "%":
+    print(" remainder is :-")
+    print(first % second)       # Perform modulo operation
+
 else:
-    print("wrong input") 
-
-
-
-
+    print("wrong input")        # Handle invalid operation input
 
